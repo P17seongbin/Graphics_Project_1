4c21afc8271f7b200f0416a72264e73013e7326b
incdux7450 <incdux7450@postech.ac.kr>
2019-06-06 22:12:07 +0900

    보여주는 범위 슬라이더 제작 중

--- 4D_Renderer/Assets/Scripts/MaterialManager.cs
+++ 4D_Renderer/Assets/Scripts/MaterialManager.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MaterialManager : MonoBehaviour
 {
     public Material Wire;
     public Material Color;
+    public Slider minBar;
+    public Slider maxBar;
 
     // Start is called before the first frame update
     void Start()
     {
         Wire = Resources.Load("Temp") as Material;
         Color = Resources.Load("showSelectedRange") as Material;
+        minBar = GameObject.Find("MinBar").GetComponent<Slider>();
+        maxBar = GameObject.Find("MaxBar").GetComponent<Slider>();
 
+        minBar.onValueChanged.AddListener(minValChange);
+        maxBar.onValueChanged.AddListener(maxValChange);
     }
 
     // Update is called once per frame
@@ -23,4 +30,15 @@ public class MaterialManager : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.F4))
             GameObject.Find("Object").GetComponent<MeshRenderer>().material = Color;
     }
+
+    private void minValChange(float val)
+    {
+        GameObject.Find("Object").GetComponent<MeshRenderer>().material.SetFloat("range.x", val);
+    }
+
+    private void maxValChange(float val)
+    {
+        GameObject.Find("Object").GetComponent<MeshRenderer>().material.SetFloat("range.y", val);
+    }
+
 }
