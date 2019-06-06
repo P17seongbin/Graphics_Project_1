d5e5f5c302f343e7ebd2a1c9e2f4ecfaa5699b18
incdux7450 <incdux7450@postech.ac.kr>
2019-06-06 21:34:14 +0900

    F3/F4로 material 변경 / material 들을 resource 폴더로 옮김

---
+++ 4D_Renderer/Assets/Scripts/MaterialManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialManager : MonoBehaviour
+{
+    public Material Wire;
+    public Material Color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Wire = Resources.Load("Temp") as Material;
+        Color = Resources.Load("showSelectedRange") as Material;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F3))
+            GameObject.Find("Object").GetComponent<MeshRenderer>().material = Wire;
+        else if (Input.GetKeyDown(KeyCode.F4))
+            GameObject.Find("Object").GetComponent<MeshRenderer>().material = Color;
+    }
+}
